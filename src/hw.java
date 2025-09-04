96fa4e51837f2149f6c8e2fce1937ded9f7230f6
sayfullo58 <sayfulloadhamjanov@gmail.com>
2025-09-04 21:20:37 +0600

    Initial commit

---
+++ src/hw.java
@@ -0,0 +1,17 @@
+public class hw {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        System.out.println(4 + 4 - 3);
+
+
+        //ПЕРЕМЕННАЯ
+        int myAge = 18;
+
+
+        System.out.println(myAge);
+        myAge = myAge + 7;
+        System.out.println(myAge);
+
+
+    }
+}
